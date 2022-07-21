820401dc0bcf405296e43d92beecb56d0529e75b
Aleksandar Radovanovic <admin@radovanovic.com>
2022-07-21 11:44:50 +0200

    rngbin-matrixvisualization

---
+++ rngbin-matrixvisualization.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+#----------------------------------------------------------------------------
+# Created By  : Aleksandar Radovanovic
+# Created Date: 2022-07-21
+# version ='1.0'
+# ---------------------------------------------------------------------------
+
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+# visualize random string as a matrix
+#--------------------------------------------
+def rngbin_matrixvisualization(rnd_seq):
+
+    # reduce to a square matrix
+    size = int(math.sqrt(len(rnd_seq)))
+    rnd_seq = rnd_seq[0:size*size]
+
+    x = np.array(list(rnd_seq.replace('0','b').replace('1','r')))
+    shape = ( size, size )
+    mat  = x.reshape( shape )
+    X,Y = np.meshgrid(np.arange(mat.shape[1]), np.arange(mat.shape[0]))
+    plt.scatter(X.flatten(), Y.flatten(), c=mat.flatten())
+    plt.title("matrix visualisation")
+    plt.show()
+    return
+
+# TEST
+#------------------------------------------------
+
+# generate a sequence of n random bits
+n = 1000
+random_seq = f'{random.getrandbits(n):=0{n}b}'
+
+# test
+rngbin_matrixvisualization(random_seq)
+
